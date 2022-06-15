7e47adda02ec602d9a99af4657b8dc21092f2757
Venki Balakrishnan <venkyvb@gmail.com>
2022-06-15 09:54:35 -0700

    Initial commit

---
+++ src/test/java/com/vbv/hl7/HL7SimpleRouteTests.java
@@ -0,0 +1,54 @@
+package com.vbv.hl7;
+
+import ca.uhn.hl7v2.model.Message;
+import ca.uhn.hl7v2.parser.DefaultXMLParser;
+import ca.uhn.hl7v2.parser.XMLParser;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.Rule;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.jupiter.api.Test;
+
+public class HL7SimpleRouteTests extends CamelTestSupport {
+
+    @Rule
+    SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+    @Override
+    protected RoutesBuilder createRouteBuilder() {
+
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("file:src/test/resources/hl7?noop=true")
+                        .convertBodyTo(String.class)
+                        .unmarshal()
+                        .hl7(false)
+                        .log("The Message body is: ${body}")
+                        .process(new Processor() {
+                            public void process(Exchange exchange) throws Exception {
+                                final Message message = exchange.getIn().getBody(Message.class);
+                                XMLParser xmlParser = new DefaultXMLParser();
+                                String xmlPayload = xmlParser.encode(message);
+                                System.out.println("XML version: " + xmlPayload);
+                            }
+                        })
+                        .to("mock:result");
+            }
+        };
+    }
+
+    @Test
+    public void executeTest() throws InterruptedException {
+        MockEndpoint mock = getMockEndpoint("mock:result");
+        mock.expectedMessageCount(1);
+        mock.expectedBodyReceived().body(Message.class);
+
+        assertMockEndpointsSatisfied();
+
+    }
+}
+
